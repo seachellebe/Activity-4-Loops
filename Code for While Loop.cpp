03c8a1bde592a2078724e4389534c2737d256d41
Michelle <225957291+seachellebe@users.noreply.github.com>
2025-09-11 01:37:26 +0800

    Create Code for While Loop.cpp

---
+++ Code for While Loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+
+	int day = 16;
+    
+	while (day > 0) {
+    	cout << day << " days remaining " << "\n";
+        day--;
+    }      
+    	cout << "It's my birthday!!" << endl; 
+    return 0;
+
+}
