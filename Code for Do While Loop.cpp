bc96c9fe369ee27d4283b49bb271dac62619319d
Michelle <225957291+seachellebe@users.noreply.github.com>
2025-09-11 01:38:11 +0800

    Create Code for Do While Loop.cpp

---
+++ Code for Do While Loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int age = 19;
+    
+    do {
+        cout << age << " years of living" << "\n";
+        age++;
+    } while(age < 19);
+        cout << "turning 20 this month";
+    
+    return 0;
+}
